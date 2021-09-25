0eb98ebd2e5221d2118062cb3ef2ed4e0e640537
Balazs <balazs@weart.hu>
2021-09-25 13:04:23 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
 // menu item hide
 const social = document.querySelectorAll('a span.uk-text-small');
 social.forEach(item => {
-    let t = String(item.innerText); // make a string from name
+    var t = String(item.innerText); // make a string from name
     t = str = t.replace(/\s+/g, ''); // remove space from name
     if( t == 'hidden' ){
         item.querySelector('span.text').style.display = 'none';
         item.querySelector('span.fab').classList.remove('uk-margin-small-right');
+    }
+});
+
+const services = document.querySelectorAll('#services .w-text');
+services.forEach(item => {
+    if(item.clientHeight > 90){
+        item.classList.add('uk-height-90');
     }
 });
